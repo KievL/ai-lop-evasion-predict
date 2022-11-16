e5035e22531c0ae7548ae1915aa89de627bf83ff
KievL <kiev.sider@hotmail.com>
2022-11-15 21:40:45 -0300

    last

--- processWebHook.py
+++ processWebHook.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 from flask import send_from_directory, Flask, render_template, request
+import json
 
 app = Flask(__name__)
 
@@ -18,25 +19,31 @@ def favicon():
 
 @app.route('/predict', methods=['POST', 'GET'])
 def predict():
-    total_errors = float(request.form['totalErrors'])
-    algum_acerto = float(request.form['algumAcerto'])
-    questoes_certas = float(request.form['questoesCertas'])
-    acertos_100 = float(request.form['acertos_100'])
-    submissoes = float(request.form['submissoes'] )
-    questoes_tentadas = float(request.form['questoesTentadas'])
+    predict_data={}
+    for d in request.args.keys():
+        val=request.args.get(d)
+        predict_data[d]=val
+    total_errors = float(predict_data['totalErrors'])
+    algum_acerto = float(predict_data['algumAcerto'])
+    questoes_certas = float(predict_data['questoesCertas'])
+    acertos_100 = float(predict_data['acertos_100'])
+    submissoes = float(predict_data['submissoes'] )
+    questoes_tentadas = float(predict_data['questoesTentadas'])
     subms_por_questao = float(submissoes/questoes_tentadas)
-    tempo_lista = float(request.form['tempoLista'])
+    tempo_lista = float(predict_data['tempoLista'])
     perc_total_error = float(total_errors/submissoes)
     perc_algum_acerto = float(algum_acerto/submissoes)
     perc_acerto_100 = float(acertos_100/submissoes)
 
     desistente = model.predict([[total_errors, algum_acerto, questoes_certas, acertos_100, submissoes, subms_por_questao, tempo_lista, perc_total_error, perc_algum_acerto, perc_acerto_100]])[0]    
-    if(desistente==0):
-        desistente='Não'
-    else:
-        desistente ='Sim'
+    #if(desistente==0):
+    #   desistente='Não'
+    #else:
+    #    desistente ='Sim'
     result = desistente
-    return render_template('index.html', **locals())
+
+    json_return = json.dumps({'desistente': int(result)})
+    return json_return
 
 if __name__ == "__main__":
     app.secret_key = 'ItIsASecret'
